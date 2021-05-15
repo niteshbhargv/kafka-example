4c3fb425c823a355bb614ba6414a18fc3dc4f890
niteshbhargv <nitesh.bhargv@gmail.com>
2021-05-16 00:01:59 +0530

    Publishing message

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 Kafka example 
 
 https://www.youtube.com/watch?v=NjHYWEV_E_o&ab_channel=TechPrimers
+
+Kafka startup commands 
+
+ZooKeeper
+bin/zookeeper-server-start.sh config/zookeeper.properties
+
+kafka server 
+
+bin/kafka-server-start.sh config/server.properties 
+
+topic creation
+
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic Kafka_example
+
+consumer 
+bin/kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic Kafka_example --from-beginning
---
+++ src/main/java/com/niteshbhargv/kafka/resource/UserResource.java
@@ -0,0 +1,23 @@
+package com.niteshbhargv.kafka.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kafka")
+public class UserResource {
+
+    @Autowired
+    KafkaTemplate<String, String> kafkaTemplate;
+
+    public static final String TOPIC = "Kafka_example";
+    @GetMapping("/publish/{message}")
+    public String publishMessage(@PathVariable("message") final String message) {
+        kafkaTemplate.send(TOPIC, message);
+        return "published successfully";
+    }
+}
